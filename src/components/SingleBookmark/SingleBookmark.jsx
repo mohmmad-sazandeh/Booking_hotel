2a86719c7fe2a8aa24c5bdef4dc242e77f8e13b4
mohammad-sazandeh <mohamad.sazandeh84@gmail.com>
2025-05-22 12:03:06 +0330

    add SingleBookmark component

---
+++ src/components/SingleBookmark/SingleBookmark.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useBookmark } from "../context/BookmarkListContext";
+import { useNavigate, useParams } from "react-router-dom";
+import Loader from "../Loader/Loader";
+
+function SingleBookmark() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { getBookmar, isLoadingCurrBookmark, currentBookmark } = useBookmark();
+  useEffect(() => {
+    getBookmar(id);
+  }, [id]);
+
+  if (isLoadingCurrBookmark || !currentBookmark) return <Loader />;
+  return (
+    <div>
+      <button onClick={() => navigate(-1)} className="btn btn--back">
+        {" "}
+        &larr; Back
+      </button>
+      <h2>{currentBookmark.cityName}</h2>
+      <div
+        className={`bookmarkItem`}
+      >
+        <ReactCountryFlag svg countryCode={currentBookmark.countryCode} />
+        &nbsp; <strong>{item.cityName}</strong> &nbsp;
+        <span>{item.country}</span>
+      </div>
+    </div>
+  );
+}
+
+export default SingleBookmark;
