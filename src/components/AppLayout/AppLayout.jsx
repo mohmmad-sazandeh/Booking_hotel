77e9f3db8ed17955deab5a6fa4b26e6e50dc2f47
mohammad-sazandeh <mohamad.sazandeh84@gmail.com>
2025-06-29 16:35:57 +0330

    Update AppLauout

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import {Outlet} from"react-router-dom";
-import Map from '../Map/map';
-import { useHotels } from '../context/HotelProvider';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Map from "../Map/map";
+import { useHotels } from "../context/HotelProvider";
 
 function AppLayout() {
-  const {hotels} = useHotels()
+  const { hotels } = useHotels();
   return (
     <div className="appLayout">
-        <div className="sidebar">
-            <Outlet />
-        </div>
-         <Map markerLocations={hotels} />
-
+      <div className="sidebar">
+        <Outlet />
+      </div>
+      <Map markerLocations={hotels} />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
